beced88dfd9843b49b87b0050f3f2c8803dfb4ed
Femi <codemorecamp@gmail.com>
2020-11-25 09:04:36 +0100

    First commit

---
+++ src/MyApp.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+function MyApp(){
+    return (
+     <div>
+        <h1>Learn More</h1>
+        <h2>This is it</h2>
+        <h3>My numbers are</h3>
+        <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      <h3>And</h3>
+     <ol>
+       <li>Three</li>
+       <li>two</li>
+       <li>one</li>
+     </ol>
+  
+    </div>
+    )
+  }
+
+ export default MyApp
